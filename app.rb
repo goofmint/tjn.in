9025cb92b2863bb52bb0e4ea94fa4bb2a7431f34
中津川 篤司 <atsushi@moongift.jp>
2009-06-04 23:18:16 +0900

    fix

--- app.rb
+++ app.rb
@@ -8,18 +8,10 @@ get '/:id' do |id|
   end
   str = id.to_i(64).to_s
   case str[-1].chr
-  when "1" # Entry
+  when "1", "2" # Entry
     entry = Entry.find_by_id str[0..-2]
     if entry && entry.person
       redirect "http://tatsuj.in/people/#{entry.person.id}-#{entry.person.name}/entries/#{entry.id}"
-    end
-  when "2" # Comment
-    comment = Comment.find_by_id str[0..-2]
-    if comment
-      case comment.commentable.class.to_s
-      when "Entry"
-        redirect "http://tatsuj.in/people/#{comment.commenter.id}-#{comment.commenter.name}/entries/#{comment.commentable.id}"
-      end
     end
   end
   redirect "http://tatsuj.in/"
